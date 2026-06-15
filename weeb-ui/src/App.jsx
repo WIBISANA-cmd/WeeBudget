5bdbe99351d145421be7dc3a8e5daa78f5e0cd45
WIBISANA-cmd <email-github-baru@example.com>
2026-06-15 20:12:56 +0700

    ref

--- weeb-ui/src/App.jsx
+++ weeb-ui/src/App.jsx
@@ -1,11 +1,9 @@
 import AppRouter from './routes/AppRouter';
 import PwaManager from './components/pwa/PwaManager';
-import ModernPreloader from './components/feedback/ModernPreloader';
 
 function App() {
   return (
     <>
-      <ModernPreloader />
       <AppRouter />
       <PwaManager />
     </>
